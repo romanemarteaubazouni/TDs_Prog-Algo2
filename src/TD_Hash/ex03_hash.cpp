a6fb9971a4079f32698a92130b16f41cb1b7d7e2
romanemarteaubazouni <romane.marteaubazouni@gmail.com>
2026-04-11 17:39:46 +0200

    finished ex bonus + understood unordered_map

--- src/TD_Hash/ex03_hash.cpp
+++ src/TD_Hash/ex03_hash.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <array>
 #include <map>
 #include <unordered_map>
 
@@ -31,8 +32,8 @@ struct Card {
     CardValue value;
 
     // Question 2/3
-    int hash() {
-        // Valeurs des couleurs (pas de 13 car 13 cartes dans une couleur)
+    int hash() const {
+        // Valeurs des couleurs (13 cartes dans une couleur)
         int hash_kind {static_cast<int>(kind) * 13};
         // Valeurs des cartes (allant de 1 à 13)
         int hash_value {static_cast<int>(value) + 1};
@@ -49,7 +50,7 @@ bool operator==(Card const& a, Card const& b) {
 namespace std {
     template<>
     struct hash<Card> {
-        size_t operator()(Card& card) const {
+        size_t operator()(Card const& card) const {
             return card.hash();
         }
     };
@@ -64,8 +65,45 @@ std::vector<Card> get_cards(size_t const size) {
     return cards;
 }
 
+std::string card_name(Card const& card) {
+    std::string name {};
+
+    unsigned int card_value {(static_cast<unsigned int>(card.value)+2) % 14};
+
+    if (card_value < 10) {
+    name += '0' + std::to_string(card_value);
+    }else if (card_value == 10) {
+    name += "10";
+    }else if (card_value == 11) {
+    name += 'J';
+    }else if (card_value == 12) {
+    name += 'Q';
+    }else if (card_value == 13) {
+    name += 'K';
+    }
+
+    name += " of ";
+
+    if (card.kind == CardKind::Heart) {
+    name += "Heart";
+    }else if (card.kind == CardKind::Diamond) {
+    name += "Diamond";
+    }else if (card.kind == CardKind::Club) {
+    name += "Club";
+    }else if (card.kind == CardKind::Spade) {
+    name += "Spade";
+    }
+    return name;
+}
+// Question 4
 int main() {
-    get_cards(100);
+    std::vector<Card> random_cards {get_cards(100)};
     std::unordered_map<Card, int> cards {};
+    for (Card const& card : random_cards) {
+        cards[card]++;
+    }
+    for (auto const& pair : cards) {
+        std::cout << "La carte " << card_name(pair.first) << " apparait " << pair.second << " fois.\n";
+    }
     return 0;
 }
